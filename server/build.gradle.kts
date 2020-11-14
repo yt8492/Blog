4ec570eb8a14004b3af41bbb662d88e30e4f43ab
yt8492 <tmymyut2000@gmail.com>
2020-11-14 23:55:02 +0900

    add klock

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(project(":common"))
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
+    implementation("com.soywiz.korlibs.klock:klock:2.0.0-alpha")
     implementation("io.ktor:ktor-server-netty:1.4.1")
     implementation("io.ktor:ktor-serialization:1.4.1")
     implementation("ch.qos.logback:logback-classic:1.2.3")
