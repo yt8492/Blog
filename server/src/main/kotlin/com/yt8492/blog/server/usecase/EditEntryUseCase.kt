2bb1d6095350656707ff9e55033423e89baf4517
yt8492 <tmymyut2000@gmail.com>
2020-11-22 11:47:00 +0900

    add EditEntryUseCase

---
+++ server/src/main/kotlin/com/yt8492/blog/server/usecase/EditEntryUseCase.kt
@@ -0,0 +1,21 @@
+package com.yt8492.blog.server.usecase
+
+import com.yt8492.blog.common.model.Entry
+import com.yt8492.blog.common.model.EntryId
+
+interface EditEntryUseCase : UseCase<EditEntryUseCase.Arg, EditEntryUseCase.Result> {
+    data class Arg(
+        val id: EntryId,
+        val title: String?,
+        val content: String?,
+        val tags: List<String>?,
+        val isPreview: Boolean?
+    )
+
+    sealed class Result {
+        data class Success(val entry: Entry) : Result()
+        sealed class Failure : Result() {
+            object EntryNotFound : Failure()
+        }
+    }
+}
