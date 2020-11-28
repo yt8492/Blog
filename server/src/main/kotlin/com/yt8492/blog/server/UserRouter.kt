a5bb8305c529cc4c8d13402a64904843407cabe5
yt8492 <tmymyut2000@gmail.com>
2020-11-28 21:39:58 +0900

    add UserRouter

---
+++ server/src/main/kotlin/com/yt8492/blog/server/UserRouter.kt
@@ -0,0 +1,17 @@
+package com.yt8492.blog.server
+
+import com.yt8492.blog.server.adapter.controller.UserController
+import com.yt8492.blog.server.adapter.json.SignInRequestJson
+import io.ktor.application.*
+import io.ktor.request.*
+import io.ktor.routing.*
+
+fun Route.userController(controller: UserController) {
+    route("/login") {
+        post {
+            val body = call.receive<SignInRequestJson>()
+            val result = controller.signIn(body)
+            call.respondResult(result)
+        }
+    }
+}
