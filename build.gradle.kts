45d957243fbc726c30bc023d85e239b985b2ac15
yt8492 <tmymyut2000@gmail.com>
2020-12-11 13:29:27 +0900

    fix main file name

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.4.20" apply false
-    kotlin("plugin.serialization") version "1.4.20" apply false
+    kotlin("multiplatform") version "1.4.21" apply false
+    kotlin("plugin.serialization") version "1.4.21" apply false
 }
 
 group = "org.yt8492"
