0137ff9cce7276cf6273e8f9d1149dd6be85f7c1
yt8492 <tmymyut2000@gmail.com>
2020-12-14 20:59:49 +0900

    delete GlobalScope

--- webfront/src/main/kotlin/ui/page/Entries.kt
+++ webfront/src/main/kotlin/ui/page/Entries.kt
@@ -3,7 +3,6 @@ package ui.page
 import api.Api
 import com.yt8492.blog.common.model.Entry
 import kotlinx.browser.document
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.css.Display
@@ -19,11 +18,9 @@ val entriesPage = functionalComponent<EntriesProps> { props ->
     val (state, setState) = useState(listOf<Entry>())
     useEffect(listOf(props.page)) {
         document.title = """Log.d("yt8492", blog)"""
-        GlobalScope.launch {
-            MainScope().launch {
-                val entries = Api.getPublicEntries(props.page)
-                setState(entries)
-            }
+        MainScope().launch {
+            val entries = Api.getPublicEntries(props.page)
+            setState(entries)
         }
     }
     styledDiv {
