fcb9fdea675bb5ce39766bb2977502699a10459e
yt8492 <tmymyut2000@gmail.com>
2021-05-19 15:36:48 +0900

    fix api base url

--- webfront/src/main/kotlin/api/Api.kt
+++ webfront/src/main/kotlin/api/Api.kt
@@ -6,11 +6,9 @@ import com.yt8492.blog.common.model.Entry
 import com.yt8492.blog.common.model.EntryId
 import io.ktor.client.*
 import io.ktor.client.engine.js.*
-import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
-import io.ktor.http.*
 
 object Api {
 
@@ -20,18 +18,15 @@ object Api {
         install(JsonFeature) {
             serializer = KotlinxSerializer()
         }
-        defaultRequest {
-            url.takeFrom(baseUrl)
-        }
     }
 
     suspend fun getPublicEntries(page: Int): List<Entry> {
-        return client.get<List<EntryResponseJson>>("/api/entries?page=$page")
+        return client.get<List<EntryResponseJson>>("$baseUrl/api/entries?page=$page")
             .map { it.toModel() }
     }
 
     suspend fun getEntryById(id: EntryId): Entry? {
-        return client.get<EntryResponseJson>("/api/entries/${id.value}")
+        return client.get<EntryResponseJson>("$baseUrl/api/entries/${id.value}")
             .toModel()
     }
 }
