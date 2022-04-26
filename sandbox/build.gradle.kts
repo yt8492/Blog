db885615c8b5b1ff7eb620cdb1de5bdd3b18f414
yt8492 <tmymyut2000@gmail.com>
2022-04-26 23:37:43 +0900

    add sandbox main

--- sandbox/build.gradle.kts
+++ sandbox/build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     kotlin("jvm")
+    application
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+}
+
+application {
+    mainClassName = ("com.yt8492.blog.sandbox.MainKt")
 }
 
 repositories {
@@ -7,6 +13,10 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":common"))
     implementation(project(":server"))
     implementation(kotlin("stdlib"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("com.google.cloud:google-cloud-datastore:2.1.0")
 }
